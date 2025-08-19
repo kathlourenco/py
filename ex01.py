63fb17ffb8c48a073e4334b53864cb3059c394c7
Kathleen Lourenço <kathclairesl@gmail.com>
2025-08-19 14:41:01 -0300

    feat: add args* conteudo

--- ex01.py
+++ ex01.py
@@ -1,22 +1,7 @@
-while True:
-    try:
-        a = int(input("Digite um número: "))
-        b = int(input("Digite um número: "))
-        if a < 0 or b < 0:
-            raise TypeError("O valor deve ser positivo")
-        c = a / b
-        print(f"Resultado: {c}")
-    except ZeroDivisionError:
-        print("Erro: ocorreu uma divisão por zero")
-    except ValueError:
-        print("O valor deve ser um número inteiro")
-    except TypeError as mensagem:
-        print(mensagem)
-    except Exception:
-        print("Erro inesperado")
-    else:
-        print("Operação realizada com sucesso")
-    finally:
-        print("Fim do programa")
-        break
-print("Continua...")
+def soma(*args):
+    print(args)
+    for a in args:
+        print(a)
+    return sum(args)
+print(f"soma: {soma(1, 2, 3)}")
+print(f"soma: {soma(4, 5, 6)}")
